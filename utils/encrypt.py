60bd74cc9da34728235730d3f05a8d4a3ffa2f4f
regular-life <yash22586@iiitd.ac.in>
2023-12-24 18:13:45 +0530

    remade virtualenv

--- utils/encrypt.py
+++ utils/encrypt.py
@@ -1,18 +1,18 @@
-from cryptography.fernet import Fernet
+# from cryptography.fernet import Fernet
 
-# Function to generate a key for encryption
-def generate_key():
-    return Fernet.generate_key()
+# # Function to generate a key for encryption
+# def generate_key():
+#     return Fernet.generate_key()
 
-# Function to encrypt data
-def encrypt_data(key, data):
-    cipher_suite = Fernet(key)
-    cipher_text = cipher_suite.encrypt(data.encode())
-    return cipher_text
+# # Function to encrypt data
+# def encrypt_data(key, data):
+#     cipher_suite = Fernet(key)
+#     cipher_text = cipher_suite.encrypt(data.encode())
+#     return cipher_text
 
-# Function to decrypt data
-def decrypt_data(key, cipher_text):
-    cipher_suite = Fernet(key)
-    plain_text = cipher_suite.decrypt(cipher_text).decode()
-    return plain_text
+# # Function to decrypt data
+# def decrypt_data(key, cipher_text):
+#     cipher_suite = Fernet(key)
+#     plain_text = cipher_suite.decrypt(cipher_text).decode()
+#     return plain_text
 
